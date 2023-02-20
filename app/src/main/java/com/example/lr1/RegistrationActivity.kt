1173230b6f5ee7b9b2708373d810579d74fbc555
FlaTyr <romka2014grossman@gmail.com>
2023-02-21 01:44:49 +0200

    LR2

--- app/src/main/java/com/example/lr1/RegistrationActivity.kt
+++ app/src/main/java/com/example/lr1/RegistrationActivity.kt
@@ -1,5 +1,7 @@
 package com.example.lr1
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.text.InputType.*
 import android.widget.Button
@@ -9,11 +11,16 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 
-class MainActivity : AppCompatActivity() {
+class RegistrationActivity : AppCompatActivity() {
+    fun sharingRefernce(login: String?, pass: String?, autoE: Boolean){
+        val shared_ref = getSharedPreferences("users", Context.MODE_PRIVATE)
+        shared_ref.edit().putString("login", login).apply()
+        shared_ref.edit().putString("password", pass).apply()
+        shared_ref.edit().putBoolean("autoLogIn", autoE).apply()
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
-/*        setContentView(R.layout.activity_registration)*/
         val reg = findViewById<Button>(R.id.button)
         val login = findViewById<EditText>(R.id.etext)
         val pass = findViewById<EditText>(R.id.etext2)
@@ -25,7 +32,7 @@ class MainActivity : AppCompatActivity() {
             var error = ""
             if ('@' !in login.text.toString() && on_mail.currentTextColor == resources.getColor(R.color.purple_200))
                 error += "нет знака @\n"
-            if ('+' !in login.text.toString()&& on_mail.currentTextColor == resources.getColor(R.color.black))
+            if ('+' !in login.text.toString() && on_mail.currentTextColor == resources.getColor(R.color.black))
                 error += "нет знака +\n"
             if (!pass.text.toString().equals(pass_re.text.toString()))
                 error += "пароли не совпадают\n"
@@ -33,7 +40,11 @@ class MainActivity : AppCompatActivity() {
                 error += "пароль <8 символов\n"
             if (!error.isEmpty())
                 Toast.makeText(this, error, Toast.LENGTH_LONG).show()
-
+            else {
+                sharingRefernce(login.text.toString(), pass.text.toString(), false)
+                val intent = Intent(this, ContentActivity::class.java)
+                startActivity(intent)
+            }
         }
         on_mail.setOnClickListener {
             on_mail.setTextColor(resources.getColor(R.color.purple_200))
@@ -54,4 +65,5 @@ class MainActivity : AppCompatActivity() {
             login.inputType = TYPE_CLASS_PHONE
         }
     }
+
 }
---
+++ app/src/main/java/com/example/lr1/SplashActivity.kt
@@ -0,0 +1,39 @@
+package com.example.lr1
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.core.content.edit
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val shared_ref = getSharedPreferences("users", Context.MODE_PRIVATE)
+
+        val pb = findViewById<ProgressBar>(R.id.progress)
+        pb.setOnClickListener() {
+            if (shared_ref.getString("login","").isNullOrEmpty()){
+                pb.progress = 100
+                val intent = Intent(this, RegistrationActivity::class.java)
+                startActivity(intent)
+            }
+            if(!shared_ref.getBoolean("autoLogIn", false)){
+                pb.progress = 100
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+            }
+            else{
+                pb.progress = 100
+                val intent = Intent(this, ContentActivity::class.java)
+                startActivity(intent)
+            }
+
+
+        }
+    }
+}
